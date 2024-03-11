b89f3e961c01efbd0a99de9e4fc0dc67202f989e
MuhammedBasilvp <basilvp7@gmail.com>
2024-03-11 19:24:16 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+    /* mobile menu */
+    $('.mobile.ham_menu').on('click', function(){ $('.ham_menu_wrapper').addClass('open'); })
+    $('.clz').on('click', function(){ $('.ham_menu_wrapper').removeClass('open'); })
+
+  
+});
+
